38ddcb81b873791a423465d7059f9e40159706c9
Ivan Trubnikov <trubnikov_work@bk.ru>
2026-03-02 23:42:49 +0300

    this version consists one thread and uart realisation with ring buffer

---
+++ system_prog/uart_project/uart.c
@@ -0,0 +1,229 @@
+// One-thread UART Simulation
+/*
+ * This file consists realisation of UART
+ * transmitting and receiving UART frames
+ * with a ring buffers
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define UART_STOP_BIT_POS  9
+#define UART_START_BIT_POS 0
+#define BUFFER_CAPACITY	   8
+
+_Static_assert(((BUFFER_CAPACITY & (BUFFER_CAPACITY - 1)) == 0),
+	       "BUFFER_CAPACITY must be a power of two!");
+
+struct uart_ring_buffer {
+	uint8_t buffer[BUFFER_CAPACITY];
+	size_t head;
+	size_t tail;
+	size_t count;
+};
+
+struct uart_device {
+	struct uart_ring_buffer buffer_rx;
+	struct uart_ring_buffer buffer_tx;
+	uint32_t baud_speed;
+	char ID;
+};
+
+struct uart_frame {
+	uint16_t data;
+};
+
+/* Ring buffer functions */
+void buffer_init(struct uart_ring_buffer *buffer)
+{
+	*buffer = (struct uart_ring_buffer) {0};
+}
+
+bool push_buffer(struct uart_ring_buffer *buffer, uint8_t data)
+{
+	if (buffer->count == BUFFER_CAPACITY) {
+		return false;
+	}
+	buffer->buffer[buffer->head] = data;
+
+	buffer->head = (buffer->head + 1) & (BUFFER_CAPACITY - 1);
+	buffer->count++;
+
+	return true;
+}
+
+bool pop_buffer(struct uart_ring_buffer *buffer, uint8_t *out_data)
+{
+	if (buffer->count == 0) {
+		return false;
+	}
+	*out_data = buffer->buffer[buffer->tail];
+	buffer->tail = (buffer->tail + 1) & (BUFFER_CAPACITY - 1);
+	buffer->count--;
+	return true;
+}
+
+/* Service functions for print information */
+void print_bin(uint32_t number, uint8_t bits)
+{
+	for (int i = bits - 1; i >= 0; i--) {
+		if (number & (1U << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+		if (i % 4 == 0)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+void print_buffer(const struct uart_ring_buffer *buffer)
+{
+	for (size_t i = 0; i < BUFFER_CAPACITY; i++) {
+		printf("[%u] ", buffer->buffer[i]);
+	}
+	printf("\n");
+}
+
+/* Device life cycle functions */
+struct uart_device *init_device(uint32_t baud_speed, char ID)
+{
+	struct uart_device *device = malloc(sizeof(struct uart_device));
+	if (!device) {
+		return NULL;
+	}
+
+	buffer_init(&device->buffer_rx);
+	buffer_init(&device->buffer_tx);
+
+	device->ID = ID;
+	device->baud_speed = baud_speed;
+
+	printf("[INIT_DEVICE]: Initialised Device %c\n[address: %p, buffer "
+	       "size: %u, baud speed: %u\n",
+	       device->ID, device, BUFFER_CAPACITY, device->baud_speed);
+	return device;
+}
+
+void destroy_device(struct uart_device **device)
+{
+	if (!*(device)) {
+		return;
+	}
+	printf("[DESTROY_DEVICE]: Destroying Device %c...\n", (*device)->ID);
+	free(*device);
+
+	*device = NULL;
+}
+
+/* UART functions */
+struct uart_frame uart_pack_frame(uint8_t data)
+{
+	struct uart_frame frame = {0};
+	frame.data |= (1U << UART_STOP_BIT_POS);
+	frame.data |= (data << 1);
+
+	printf("[PACK_FRAME]: Created UART Frame from original data: ");
+	print_bin(data, 8);
+	printf("[PACK_FRAME]: UART Frame: ");
+	print_bin(frame.data, 16);
+
+	return frame;
+}
+
+bool uart_unpack_frame(struct uart_frame frame, uint8_t *rx_data)
+{
+	// if we dont have stop bit or start bit - our frame is corrupted
+	if (!(frame.data & (1U << UART_STOP_BIT_POS)) ||
+	    frame.data & (1U << UART_START_BIT_POS)) {
+		printf("[UNPACK_FRAME]: Error, incorrect frame\n");
+		return false;
+	}
+
+	*rx_data = (frame.data >> 1) & 0xFF;
+	return true;
+}
+
+void uart_transmit(struct uart_device *tx, struct uart_device *rx)
+{
+	// checking correctness of devices and baud speed
+	if (!tx) {
+		printf("[UART_TRANSMIT]: Error, invalid TX device\n");
+		return;
+	}
+	if (!rx) {
+		printf("[UART_TRANSMIT]: Error, invalid RX device\n");
+		return;
+	}
+	if (rx->baud_speed != tx->baud_speed) {
+		printf("[UART_TRANSMIT]: Error, not same baud speed\n");
+		return;
+	}
+	// tx procedure
+	uint8_t tx_data = 0;
+	if (!(pop_buffer(&tx->buffer_tx, &tx_data))) {
+		printf("[UART_TRANSMIT]: Error, can not pop the data from TX "
+		       "Buffer of Device %c\n",
+		       tx->ID);
+		return;
+	}
+	struct uart_frame tx_frame = uart_pack_frame(tx_data);
+
+	printf("[UART_TRANSMIT]: Device %c transmitted the data to Device %c\n",
+	       tx->ID, rx->ID);
+	printf("[UART_TRANSMIT]: TX Buffer of Device %c: ", tx->ID);
+	print_buffer(&tx->buffer_tx);
+
+	// rx procedure
+	uint8_t rx_data = 0;
+	if (uart_unpack_frame(tx_frame, &rx_data) &&
+	    push_buffer(&rx->buffer_rx, rx_data)) {
+		printf("[UART_TRANSMIT]: Device %c received the data from "
+		       "Device %c\n",
+		       rx->ID, tx->ID);
+		printf("[UART_TRANSMIT]: RX Buffer of Device %c: ", rx->ID);
+		print_buffer(&rx->buffer_rx);
+	} else {
+		printf("[UART_TRANSMIT]: Error receiving data. Corrupted frame "
+		       "or full RX Buffer\n");
+	}
+}
+
+void uart_put_tx_data(struct uart_device *device, uint8_t data)
+{
+	if (!push_buffer(&device->buffer_tx, data)) {
+		printf("[PUT_TX]: Error, TX Buffer of Device %c is full\n",
+		       device->ID);
+		return;
+	}
+	printf(
+	    "[PUT_TX]: Successfully added data into tx_buffer of Device %c\n",
+	    device->ID);
+}
+
+/* Testing realisation */
+int main(void)
+{
+	const uint32_t baud_speed = 9600;
+	struct uart_device *device_a = init_device(baud_speed, 'A');
+	struct uart_device *device_b = init_device(baud_speed, 'B');
+
+	uart_put_tx_data(device_a, 52);
+	uart_put_tx_data(device_a, 1);
+	uart_put_tx_data(device_a, 2);
+
+	uart_transmit(device_a, device_b);
+	uart_transmit(device_a, device_b);
+	uart_transmit(device_a, device_b);
+
+	uart_put_tx_data(device_a, 7);
+	uart_transmit(device_a, device_b);
+
+	destroy_device(&device_a);
+	destroy_device(&device_b);
+
+	return 0;
+}
