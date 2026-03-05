228d72bd49d45ba3ceff9864a94ad1bda3680d8b
Ivan Trubnikov <trubnikov_work@bk.ru>
2026-03-05 19:34:04 +0300

    rewrited ring buffer logic, renamed variables, compiled with angry gcc flags!

--- system_prog/uart_project/uart.c
+++ system_prog/uart_project/uart.c
@@ -1,4 +1,4 @@
-// One-thread UART User-Space Simulation
+// UART User-Space Simulation
 /*
  * This file consists realisation of UART
  * transmitting and receiving UART frames
@@ -14,23 +14,23 @@
 #define UART_START_BIT_POS 0
 #define BUFFER_CAPACITY	   4
 
-_Static_assert(((BUFFER_CAPACITY & (BUFFER_CAPACITY - 1)) == 0),
+#define IS_POWER_OF_TWO(x) (((x) & ((x) - 1)) == 0)
+_Static_assert(IS_POWER_OF_TWO(BUFFER_CAPACITY),
 	       "BUFFER_CAPACITY must be a power of two!");
 
 typedef enum {
 	UART_SUCCESS = 0,
-	UART_ERROR_OVERFLOW,
-	UART_ERROR_EMPTY,
+	UART_ERROR_BUFFER_OVERFLOW,
+	UART_ERROR_BUFFER_EMPTY,
 	UART_ERROR_NULLPTR,
 	UART_ERROR_FRAME,
 	UART_ERROR_BAUDRATE
 } uart_status_t;
 
 struct uart_ring_buffer {
-	uint8_t buffer[BUFFER_CAPACITY];
-	volatile uint32_t head;
-	volatile uint32_t tail;
-	volatile uint32_t count;
+	volatile uint8_t rb[BUFFER_CAPACITY];
+	volatile size_t head;
+	volatile size_t tail;
 };
 
 struct uart_device {
@@ -44,48 +44,53 @@ struct uart_frame {
 	uint16_t data;
 };
 
-/* Ring buffer functions */
-uart_status_t buffer_init(struct uart_ring_buffer *buffer)
+/* Ring rb functions */
+static uart_status_t buffer_init(struct uart_ring_buffer *rb)
 {
-	if (!buffer)
-		return UART_ERROR_NULLPTR;
-	*buffer = (struct uart_ring_buffer) {0};
-	return UART_SUCCESS;
-}
-
-uart_status_t push_buffer(struct uart_ring_buffer *buffer, uint8_t data)
-{
-	if (!buffer) {
+	if (!rb) {
 		return UART_ERROR_NULLPTR;
 	}
-	if (buffer->count == BUFFER_CAPACITY) {
-		return UART_ERROR_OVERFLOW;
-	}
-	buffer->buffer[buffer->head] = data;
 
-	buffer->head = (buffer->head + 1) & (BUFFER_CAPACITY - 1);
-	buffer->count++;
+	rb->head = 0;
+	rb->tail = 0;
 
 	return UART_SUCCESS;
 }
 
-uart_status_t pop_buffer(struct uart_ring_buffer *buffer, uint8_t *out_data)
+static uart_status_t push_buffer(struct uart_ring_buffer *rb, uint8_t data)
 {
-	if (!buffer || !out_data) {
+	if (!rb) {
 		return UART_ERROR_NULLPTR;
 	}
-	if (buffer->count == 0) {
-		return UART_ERROR_EMPTY;
+	size_t new_head = (rb->head + 1) & (BUFFER_CAPACITY - 1);
+	if (new_head == rb->tail) {
+		return UART_ERROR_BUFFER_OVERFLOW;
 	}
-	*out_data = buffer->buffer[buffer->tail];
-	buffer->tail = (buffer->tail + 1) & (BUFFER_CAPACITY - 1);
-	buffer->count--;
+
+	rb->rb[rb->head] = data;
+	rb->head = new_head;
+
+	return UART_SUCCESS;
+}
+
+static uart_status_t pop_buffer(struct uart_ring_buffer *rb, uint8_t *out_data)
+{
+	if (!rb || !out_data) {
+		return UART_ERROR_NULLPTR;
+	}
+	if (rb->tail == rb->head) {
+		return UART_ERROR_BUFFER_EMPTY;
+	}
+
+	size_t new_tail = (rb->tail + 1) & (BUFFER_CAPACITY - 1);
+	*out_data = rb->rb[rb->tail];
+	rb->tail = new_tail;
 
 	return UART_SUCCESS;
 }
 
 /* Service functions for print information */
-void print_bin(uint32_t number, uint8_t bits)
+static void print_bin(uint32_t number, uint8_t bits)
 {
 	for (int i = bits - 1; i >= 0; i--) {
 		if (number & (1U << i)) {
@@ -99,22 +104,22 @@ void print_bin(uint32_t number, uint8_t bits)
 	printf("\n");
 }
 
-void print_buffer(const struct uart_ring_buffer *buffer)
+static void print_buffer(const volatile struct uart_ring_buffer *rb)
 {
 	for (size_t i = 0; i < BUFFER_CAPACITY; i++) {
-		printf("[%u] ", buffer->buffer[i]);
+		printf("[%u] ", rb->rb[i]);
 	}
 	printf("\n");
 }
 
-const char *uart_status_to_str(uart_status_t status)
+static const char *uart_status_to_str(uart_status_t status)
 {
 	switch (status) {
 	case UART_SUCCESS:
 		return "SUCCESS";
-	case UART_ERROR_OVERFLOW:
+	case UART_ERROR_BUFFER_OVERFLOW:
 		return "ERROR_OVERFLOW";
-	case UART_ERROR_EMPTY:
+	case UART_ERROR_BUFFER_EMPTY:
 		return "ERROR_EMPTY";
 	case UART_ERROR_NULLPTR:
 		return "ERROR_NULLPTR";
@@ -128,42 +133,30 @@ const char *uart_status_to_str(uart_status_t status)
 }
 
 /* Device life cycle functions */
-struct uart_device *init_device(uint32_t baud_speed, char ID)
+static struct uart_device *init_device(struct uart_device *device,
+				       uint32_t baud_speed, char ID)
 {
-	struct uart_device *device = malloc(sizeof(struct uart_device));
 	if (!device) {
 		return NULL;
 	}
-
 	if (buffer_init(&device->buffer_rx) != UART_SUCCESS ||
 	    buffer_init(&device->buffer_tx) != UART_SUCCESS) {
-		printf("[INIT_DEVICE]: Error initialisation buffer\n");
-		free(device);
+		printf("[INIT_DEVICE]: Error initialisation rb\n");
 		return NULL;
 	}
 
 	device->ID = ID;
 	device->baud_speed = baud_speed;
 
-	printf("[INIT_DEVICE]: Initialised Device %c - [address: %p, buffer "
+	printf("[INIT_DEVICE]: Initialised Device %c - [address: %p, rb "
 	       "size: %u, baud speed: %u]\n",
-	       device->ID, device, BUFFER_CAPACITY, device->baud_speed);
+	       device->ID, (void *) device, BUFFER_CAPACITY,
+	       device->baud_speed);
 	return device;
 }
 
-void destroy_device(struct uart_device **device)
-{
-	if (!*(device)) {
-		return;
-	}
-	printf("[DESTROY_DEVICE]: Destroying Device %c...\n", (*device)->ID);
-	free(*device);
-
-	*device = NULL;
-}
-
 /* UART functions */
-struct uart_frame uart_pack_frame(uint8_t data)
+static struct uart_frame uart_pack_frame(uint8_t data)
 {
 	struct uart_frame frame = {0};
 	frame.data |= (1U << UART_STOP_BIT_POS);
@@ -177,20 +170,24 @@ struct uart_frame uart_pack_frame(uint8_t data)
 	return frame;
 }
 
-uart_status_t uart_unpack_frame(struct uart_frame frame, uint8_t *rx_data)
+static uart_status_t uart_unpack_frame(struct uart_frame frame,
+				       uint8_t *rx_data)
 {
 	// if we dont have stop bit or start bit - our frame is corrupted
-	if (!(frame.data & (1U << UART_STOP_BIT_POS)) ||
-	    frame.data & (1U << UART_START_BIT_POS)) {
+	bool stop_bit_ok = frame.data & (1U << UART_STOP_BIT_POS);
+	bool start_bit_ok = !(frame.data & (1U << UART_START_BIT_POS));
+
+	if (!stop_bit_ok || !start_bit_ok) {
 		printf("[UNPACK_FRAME]: Error, incorrect frame\n");
 		return UART_ERROR_FRAME;
 	}
 
-	*rx_data = (frame.data >> 1) & 0xFF;
+	*rx_data = (uint8_t) ((frame.data >> 1) & 0xFF);
 	return UART_SUCCESS;
 }
 
-uart_status_t uart_transmit(struct uart_device *tx, struct uart_device *rx)
+static uart_status_t uart_transmit(struct uart_device *tx,
+				   struct uart_device *rx)
 {
 	// checking correctness of devices and baud speed
 	if (!tx) {
@@ -238,7 +235,7 @@ uart_status_t uart_transmit(struct uart_device *tx, struct uart_device *rx)
 		printf(
 		    "[UART_TRANSMIT]: Error, RX Buffer of Device %c is full\n",
 		    rx->ID);
-		return UART_ERROR_OVERFLOW;
+		return UART_ERROR_BUFFER_OVERFLOW;
 	}
 	printf("[UART_TRANSMIT]: Device %c received the data from "
 	       "Device %c\n",
@@ -248,7 +245,7 @@ uart_status_t uart_transmit(struct uart_device *tx, struct uart_device *rx)
 
 	return UART_SUCCESS;
 }
-uart_status_t uart_read_data(struct uart_device *rx, uint8_t *data)
+static uart_status_t uart_read_data(struct uart_device *rx, uint8_t *data)
 {
 	if (!rx) {
 		printf("[UART_READ]: Error, invalid RX device - %s\n",
@@ -265,7 +262,7 @@ uart_status_t uart_read_data(struct uart_device *rx, uint8_t *data)
 	}
 	return UART_SUCCESS;
 }
-uart_status_t uart_put_tx_data(struct uart_device *device, uint8_t data)
+static uart_status_t uart_put_tx_data(struct uart_device *device, uint8_t data)
 {
 	uart_status_t status = push_buffer(&device->buffer_tx, data);
 	if (status != UART_SUCCESS) {
@@ -284,30 +281,29 @@ uart_status_t uart_put_tx_data(struct uart_device *device, uint8_t data)
 int main(void)
 {
 	const uint32_t baud_speed = 9600;
-	struct uart_device *device_a = init_device(baud_speed, 'A');
-	struct uart_device *device_b = init_device(baud_speed, 'B');
+	struct uart_device device_a, device_b;
+	init_device(&device_a, baud_speed, 'A');
+	init_device(&device_b, baud_speed, 'B');
 
-	uart_put_tx_data(device_a, 52);
-	uart_put_tx_data(device_a, 1);
-	uart_put_tx_data(device_a, 2);
+	uart_put_tx_data(&device_a, 52);
+	uart_put_tx_data(&device_a, 1);
+	uart_put_tx_data(&device_a, 2);
 
-	uart_transmit(device_a, device_b);
-	uart_transmit(device_a, device_b);
+	uart_transmit(&device_a, &device_b);
+	uart_transmit(&device_a, &device_b);
 
 	uint8_t received_byte = 0;
-	uart_read_data(device_b, &received_byte);
-	printf("[MAIN]: Device %c proccessed byte: %u\n", device_b->ID,
+	uart_read_data(&device_b, &received_byte);
+	printf("[MAIN]: Device %c proccessed byte: %u\n", device_b.ID,
 	       received_byte);
-	uart_read_data(device_b, &received_byte);
-	printf("[MAIN]: Device %c proccessed byte: %u\n", device_b->ID,
+	uart_read_data(&device_b, &received_byte);
+	printf("[MAIN]: Device %c proccessed byte: %u\n", device_b.ID,
 	       received_byte);
 
-	uart_transmit(device_a, device_b);
+	uart_transmit(&device_a, &device_b);
 
-	uart_put_tx_data(device_a, 7);
-	uart_transmit(device_a, device_b);
+	uart_put_tx_data(&device_a, 7);
+	uart_transmit(&device_a, &device_b);
 
-	destroy_device(&device_a);
-	destroy_device(&device_b);
 	return 0;
 }
